7a620e53b01f81227f995a77270fc1fb09ead1d3
Alan75005 <alyv54@me.com>
2026-09-20 14:57:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sevigne-agenda-v79';
-const CORE=['./','./index.html','./style.css?v=79','./app.js?v=79','./manifest.webmanifest'];
+const CACHE='sevigne-agenda-v80';
+const CORE=['./','./index.html','./style.css?v=80','./app.js?v=80','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>Promise.all(CORE.map(u=>c.add(u).catch(()=>null)))));
